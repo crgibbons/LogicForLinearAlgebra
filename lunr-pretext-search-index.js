3a679ff119049f73c478dc44d9a4add715c1f485
Courtney Gibbons <crgibbon+github@hamilton.edu>
2024-09-12 17:46:55 +0000

    Latest build deployed.

---
+++ lunr-pretext-search-index.js
@@ -0,0 +1,149 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "frontmatter-2",
+  "level": "1",
+  "url": "frontmatter-2.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": "  My Website   copyright  "
+},
+{
+  "id": "ch-introduction",
+  "level": "1",
+  "url": "ch-introduction.html",
+  "type": "Chapter",
+  "number": "1",
+  "title": "Introduction",
+  "body": " Introduction  In many mathematics courses, students learn how to apply known, established mathematical facts to problems that need solutions: \"Farmer Bill wants to build a rectangular pen for his sheep with 25 yards of fence material, and he would like to maximize their grazing potential. Find the dimensions of the pen he should build.\" This type of education is important, but it is also incomplete. It does not help a student learn to create mathematics.  In contrast, mathematical reasoning, built upon the foundation of formal logic, is the art of deducing consequences from basic assumptions. Over time, these consequences (called theorems among other names) invoke fewer of the basic assumptions (called axioms ) and instead rely on secondary axioms, defintions, and other theorems. As a body, mathematicians have agreed upon a specialized dialect designed to clearly and precisely describe mathematical ideas to one another. In a very small nutshell, this is why mathematicians write proofs.  This short primer is designed to introduce this dialect, built upon formal logic, so that fledgling mathematicians may begin to discover, prove, and communicate new mathematics. Moreover, it is a work in progress by Professor Gibbons; if you have comments, criticisms, or suggestions for improvement, don't hesitate to let her know.  "
+},
+{
+  "id": "sec-ch1-not-and-or",
+  "level": "1",
+  "url": "sec-ch1-not-and-or.html",
+  "type": "Section",
+  "number": "2.1",
+  "title": "First Connectives: Not, And, and Or",
+  "body": " First Connectives: Not, And, and Or  In this section we define three logical connectives.     logical negation    The negation of a statement , denoted , is the statement: \"It is not the case that .\"  In casual conversation, we refer to the negation of a proposition as \"not \".  If we know the truth value of , then has exactly the opposite truth value. We denote this relationship with a truth table . In fact, we can take the truth table to be the defintion of the negation connective.   Truth Table for Negation    T F  F T       Negating statements  The negation of the statement, \"Hamilton College is located in Clinton, NY\" is the statement, \"It is not the case that Hamilton College is located in Clinton, NY,\" or, \"Hamilton College is not located in Clinton, NY.\"   Some logical connectives take two statements. The first of these is the logical \"and\" which we call conjunction.     logical cojunction    The conjunction of two statements and , denoted , is the statement, \" and .\"  In casual conversation, we refer to the conjunction as \" and \".  Conjunction is defined by the truth table below.   Truth Table for Conjunction    T T T  T F F  F T F  F F F       Consider the unambiguously false statement, \"A banana is a natural number.\" Then the statement, \"A banana is a natural number and \" is false no matter what statement follows the \"and.\" In order for a conjunction of two statements to be true, both original statements must be true.   The next connective is the logical \"or\" which we call disjunction.     logical disjunction    The disjunction of two statements and , denoted , is the statement, \" or .\"  In casual conversation, we refer to the disjunction as \" or \".  Disjunction is defined by the truth table below.   Truth Table for Disjunction    T T T  T F T  F T T  F F F       Consider the unambiguously false statement, \"A banana is a natural number.\" Then the statement, \"A banana is a natural number and \" is only false if the statement is false.   "
+},
+{
+  "id": "sec-ch1-not-and-or-3",
+  "level": "2",
+  "url": "sec-ch1-not-and-or.html#sec-ch1-not-and-or-3",
+  "type": "Definition",
+  "number": "2.1.1",
+  "title": "",
+  "body": "   logical negation    The negation of a statement , denoted , is the statement: \"It is not the case that .\"  In casual conversation, we refer to the negation of a proposition as \"not \".  If we know the truth value of , then has exactly the opposite truth value. We denote this relationship with a truth table . In fact, we can take the truth table to be the defintion of the negation connective.   Truth Table for Negation    T F  F T     "
+},
+{
+  "id": "sec-ch1-not-and-or-4",
+  "level": "2",
+  "url": "sec-ch1-not-and-or.html#sec-ch1-not-and-or-4",
+  "type": "Example",
+  "number": "2.1.3",
+  "title": "Negating statements.",
+  "body": " Negating statements  The negation of the statement, \"Hamilton College is located in Clinton, NY\" is the statement, \"It is not the case that Hamilton College is located in Clinton, NY,\" or, \"Hamilton College is not located in Clinton, NY.\"  "
+},
+{
+  "id": "sec-ch1-not-and-or-6",
+  "level": "2",
+  "url": "sec-ch1-not-and-or.html#sec-ch1-not-and-or-6",
+  "type": "Definition",
+  "number": "2.1.4",
+  "title": "",
+  "body": "   logical cojunction    The conjunction of two statements and , denoted , is the statement, \" and .\"  In casual conversation, we refer to the conjunction as \" and \".  Conjunction is defined by the truth table below.   Truth Table for Conjunction    T T T  T F F  F T F  F F F     "
+},
+{
+  "id": "sec-ch1-not-and-or-7",
+  "level": "2",
+  "url": "sec-ch1-not-and-or.html#sec-ch1-not-and-or-7",
+  "type": "Example",
+  "number": "2.1.6",
+  "title": "",
+  "body": " Consider the unambiguously false statement, \"A banana is a natural number.\" Then the statement, \"A banana is a natural number and \" is false no matter what statement follows the \"and.\" In order for a conjunction of two statements to be true, both original statements must be true.  "
+},
+{
+  "id": "sec-ch1-not-and-or-9",
+  "level": "2",
+  "url": "sec-ch1-not-and-or.html#sec-ch1-not-and-or-9",
+  "type": "Definition",
+  "number": "2.1.7",
+  "title": "",
+  "body": "   logical disjunction    The disjunction of two statements and , denoted , is the statement, \" or .\"  In casual conversation, we refer to the disjunction as \" or \".  Disjunction is defined by the truth table below.   Truth Table for Disjunction    T T T  T F T  F T T  F F F     "
+},
+{
+  "id": "sec-ch1-not-and-or-10",
+  "level": "2",
+  "url": "sec-ch1-not-and-or.html#sec-ch1-not-and-or-10",
+  "type": "Example",
+  "number": "2.1.9",
+  "title": "",
+  "body": " Consider the unambiguously false statement, \"A banana is a natural number.\" Then the statement, \"A banana is a natural number and \" is only false if the statement is false.  "
+},
+{
+  "id": "ch-chapter-1-connectives-7",
+  "level": "1",
+  "url": "ch-chapter-1-connectives-7.html",
+  "type": "Section",
+  "number": "2.2",
+  "title": "Compound Statements",
+  "body": " Compound Statements  Negation is a function of one binary variable (that is, we \"plug in\" one statement). Conjunctions and disjunctions are functions of two binary variables (that is, we \"plug in\" two statements). We can compose logical connectives just as we can compose functions to create compound statements .  Consider the statement : \"It is not the case that ( and ).\"   Truth Table for    T T T F  T F F T  F T F T  F F F T    For the statement to be true, we see that we need one of or to be false (not necessarily both). That is, the truth value should match the truth value of the compound statement . To verify this, we look at the truth table for the statement : \"(Not ) or (not ).\"   Truth Table for   (\\lnot p)\\lor (\\lnot q)  T T F F F  T F F T T  F T T F T  F F T T T    We can see from the final column of these truth tables that the statements and correspond to the same function of and .     logical equivalence    In general, a compound statement is a complex statement built from simple statements , , and so on. We typically reserve the capital letters , , (and so on) to denote compound statements. Two compound statements and (made up of the same constituent statements) are logically equivalent provided their truth tables have the same final column. We denote this equivalence .    Logical equivalence allows us to manipulate compound statements without changing their truth tables.  "
+},
+{
+  "id": "ch-chapter-1-connectives-7-2",
+  "level": "2",
+  "url": "ch-chapter-1-connectives-7.html#ch-chapter-1-connectives-7-2",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "compound statements "
+},
+{
+  "id": "ch-chapter-1-connectives-7-4",
+  "level": "2",
+  "url": "ch-chapter-1-connectives-7.html#ch-chapter-1-connectives-7-4",
+  "type": "Table",
+  "number": "2.2.1",
+  "title": "Truth Table for <span class=\"process-math\">\\(\\lnot(p\\land q)\\)<\/span>",
+  "body": " Truth Table for    T T T F  T F F T  F T F T  F F F T   "
+},
+{
+  "id": "ch-chapter-1-connectives-7-6",
+  "level": "2",
+  "url": "ch-chapter-1-connectives-7.html#ch-chapter-1-connectives-7-6",
+  "type": "Table",
+  "number": "2.2.2",
+  "title": "Truth Table for <span class=\"process-math\">\\((\\lnot p)\\lor (\\lnot q)\\)<\/span>",
+  "body": " Truth Table for   (\\lnot p)\\lor (\\lnot q)  T T F F F  T F F T T  F T T F T  F F T T T   "
+},
+{
+  "id": "def-",
+  "level": "2",
+  "url": "ch-chapter-1-connectives-7.html#def-",
+  "type": "Definition",
+  "number": "2.2.3",
+  "title": "",
+  "body": "   logical equivalence    In general, a compound statement is a complex statement built from simple statements , , and so on. We typically reserve the capital letters , , (and so on) to denote compound statements. Two compound statements and (made up of the same constituent statements) are logically equivalent provided their truth tables have the same final column. We denote this equivalence .   "
+},
+{
+  "id": "backmatter-2",
+  "level": "1",
+  "url": "backmatter-2.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": " This book was authored in PreTeXt .  "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
